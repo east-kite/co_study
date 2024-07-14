de2a4b809292fe984e1814c287ef955d612bd665
east-kite <east.kaito.41@gmail.com>
2024-07-14 12:02:44 +0900

    add record_page

--- lib/widgets/record_page.dart
+++ lib/widgets/record_page.dart
@@ -1,14 +1,88 @@
 import 'package:flutter/material.dart';
 
-class RecordPage extends StatelessWidget {
+class RecordPage extends StatefulWidget {
+  @override
+  _RecordPageState createState() => _RecordPageState();
+}
+
+class _RecordPageState extends State<RecordPage> {
+  TextEditingController _commentController = TextEditingController();
+
+  @override
+  void dispose() {
+    _commentController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('記録'), // ここにグループ名を動的に設定する必要があります
+      appBar: AppBar(
+        title: const Text('記録'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border:
+                    Border.all(color: const Color.fromARGB(255, 180, 178, 178)),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: DropdownButton<String>(
+                value: 'プリント(物理)',
+                onChanged: (String? newValue) {},
+                items: <String>['プリント(物理)', '参考書(物理)', '過去問(物理)']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: SizedBox(
+                      width: 200,
+                      child: Row(
+                        children: <Widget>[
+                          Image.asset('images/book.png'),
+                          const SizedBox(width: 10),
+                          Text(value),
+                        ],
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: SizedBox(
+                width: 350,
+                child: TextField(
+                  controller: _commentController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'コメントを入力',
+                  ),
+                  maxLines: 10,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  _commentController.clear(); // コメントクリア
+                },
+                child: const Text('投稿'),
+              ),
+            ),
+          ],
         ),
-        body: Center(
-          child: Text('Record Page'),
-        ));
+      ),
+    );
   }
 }
